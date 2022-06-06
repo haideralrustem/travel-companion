7b70272054e08ff1bf10c36747c0b87ecb2f0335
haideralrustem <haideralrustem@yahoo.com>
2022-06-06 17:37:08 -0500

    first

--- travel-companion/src/App.js
+++ travel-companion/src/App.js
@@ -11,18 +11,11 @@ import Map from './components/Map/Map';
 function App() {
   return (
     <div>
-      <CssBaseline />
+            
       <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
-        <Grid item xs={12} md={4}>
-          <List />
-        </Grid>
 
-        <Grid item xs={12} md={8}>
-          <Map/>
-        </Grid>
-
-      </Grid>
+      <List />
+      <Map />
     </div>
   );
 }
--- travel-companion/src/components/Header/Header.jsx
+++ travel-companion/src/components/Header/Header.jsx
@@ -1,39 +1,91 @@
 import React from 'react'
 import { Autocomplete } from '@react-google-maps/api'
 import {AppBar, Toolbar, Typography , InputBase , Box } from '@material-ui/core';
+import { styled, alpha } from '@mui/material/styles';
+import {IconButton, TextField} from "@mui/material";
 import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles';
+
+import './Header.css'
 
 export default function Header() {
 
-  
-    const classes = useStyles();
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+         marginLeft: '100%',
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
 
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          transition: theme.transitions.create('width'),
+          width: '100%',
+          [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+              width: '20ch',
+            },
+          },
+        },
+      }));
+
+
+    
     return (
-        <AppBar postion="static">
-            <Toolbar className={classes.toolbar}>
-                <Typography variant="h5" className={classes.title}>
-                    Travel Companion
-                </Typography>
-                
-                <Box display="flex">
-                    <Typography variant="h5" className={classes.title}>
-                        Explore New Places
-                    </Typography>
-                    
-                    {/* <Autocomplete> */}
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder='Search ... ' classes={{ root: classes.inputRoot, 
-                                input: classes.inputInput }}/>
-                        </div>
-                    {/* </Autocomplete> */}
-                </Box>
+        <div >
+            <Box sx={{ }}>
+                <AppBar position="static">
+                    <Toolbar className='mui-toolbar'>
 
-            </Toolbar>
-        </AppBar>
+                        <Typography variant="h5" noWrap component="div" 
+                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+                            Travel Companion
+                        </Typography>
+
+                        <Typography variant="h5" >
+                            Expolre new places
+                        </Typography>
+
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+
+                    </Toolbar>
+                </AppBar>
+            </Box>
+                
+            
+        </div>
     )
     }
